a6a2b72a925666b8a1170733ecbddcf157596321
saquib <saquibshahid508@gmail.com>
2024-07-18 19:01:36 +0530

    hnbn

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+fn main() {
+    // println!("Hello, world!");
+
+    // Definition
+    // let x = 1.5;
+    // print!("value of x is {x}");
+
+    // Mutability
+    // let y = 1.5;
+    // y = 10; // error as Y is immutable
+    // let mut y = 1;
+    // y = 10;
+
+    //Scope
+    // {
+    //     let z = 10;
+    // }
+    // let s = z; // error as variables are block scoped
+
+    //Shadowing
+    // let u = 10;
+    // let u = 1.5;
+
+    // let k = 5;
+    // {
+    //     let k = 0.3;
+    // }
+
+    //constants
+    // const MAX_VAL: i32 = 1000;
+
+    // Data Types in Rust
+    // let si: i8 = 5 ; // signed integer , i16, i32 , i64 , i128
+    // let ui : u8 = 5 ; // unsigned integer , u16, u32, u64 , u128
+
+    // let float_num = 1.5;
+
+    // let ch: char = 'c';
+    // let bl: bool = false;
+
+    // type Aliasing
+    // type Age = u8;
+    // let myAge: Age = 18;
+
+    // //type conversion
+    // let a = 10;
+    // let b = (a as f64) + 10.5;
+    // print!("{b}");
+}
